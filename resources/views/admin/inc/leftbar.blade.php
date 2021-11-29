e7f5f15be8a3be1735b92d4e4c87aaddf76edd31
K. M. Mostakim <mimrzs2013@gmail.com>
2021-11-29 14:56:12 +0600

    category crud

--- resources/views/admin/inc/leftbar.blade.php
+++ resources/views/admin/inc/leftbar.blade.php
@@ -1,5 +1,5 @@
  <!-- ########## START: LEFT PANEL ########## -->
-    <div class="sl-logo"><a href=""><i class="icon ion-android-star-outline"></i> starlight</a></div>
+    <div class="sl-logo"><a href="{{ route('admin.dashboard') }}"><i class="icon ion-android-star-outline"></i> ShopMama</a></div>
     <div class="sl-sideleft">
       <div class="sl-sideleft-menu">
 
@@ -10,7 +10,7 @@
           </div><!-- menu-item -->
         </a><!-- sl-menu-link -->
 
-        <a href="index.html" class="sl-menu-link">
+        <a href="{{ route('admin.dashboard') }}" class="sl-menu-link">
           <div class="sl-menu-item">
             <i class="menu-item-icon icon ion-ios-home-outline tx-22"></i>
             <span class="menu-item-label">Dashboard</span>
